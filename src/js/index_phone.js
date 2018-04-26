5c39b320e60a9151d43f5c83f962934ddb396bae
Oiszero <172682271kzm@gmail.com>
2018-04-26 19:59:22 +0800

    no message

--- src/js/index_phone.js
+++ src/js/index_phone.js
@@ -12,6 +12,8 @@ $(function() {
             showActiveTooltip: true,
             slidesNavigation: true,
             slidesNavPosition: 'bottom',
+            scrollBar: true,//是否包含滚动条，默认为false,若为true浏览器自带滚动条出现
+            scrollOverflow: true,//内容超过满屏后是否显示滚动条，true则显示滚动条，若需滚动查看内容还需要jquery.slimscroll插件的配合
             scrollingSpeed: 500,
             controlArrows:true,
 			dragAndMove:true,
