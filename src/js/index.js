0c61968adc93b8d8e8227f5570dada4c3513d47b
Oiszero <172682271kzm@gmail.com>
2018-04-27 09:43:39 +0800

    no message

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,27 @@ $(document).ready(function() {
 		showActiveTooltip: true,
 		slidesNavigation: true,
 		slidesNavPosition: 'bottom',
-  })
-	
-});
+      scrollBar: true,//是否包含滚动条，默认为false,若为true浏览器自带滚动条出现
+      scrollOverflow: true,//内容超过满屏后是否显示滚动条，true则显示滚动条，若需滚动查看内容还需要jquery.slimscroll插件的配合
+      scrollingSpeed: 500,
+      controlArrows:true,
+      dragAndMove:true,
+      loopBottom:true,
+      scrollOverflow:true,
+      onLeave: function (index, nextIndex, direction) {
+          console.log(index, nextIndex)
+          if (nextIndex == 2 ) {
+$(".OnlyFourSshow").show()
+          if (index > nextIndex) {
+              $('#current_num').html(index - 1);}
+          } else {
+              $('#current_num').html(nextIndex);
+          }
+      }
+  });
+    $('.next_page').on('click', function () {
+        $.fn.fullpage.moveSectionDown();
+    })
+
+
+})
