7a056ab4b9d70069b66eae053d1a7c76902d1084
Oiszero <172682271kzm@gmail.com>
2018-04-27 09:50:31 +0800

    no message

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,45 @@
-$(document).ready(function() {
-  $('#fullpage').fullpage({
-		//Navigation
-		menu: '#menu',
-		lockAnchors: true,
-		anchors:['firstPage', 'secondPage'],
-		navigation: true,
-		navigationPosition: 'right',
-		navigationTooltips: ['firstSlide', 'secondSlide', 'thirdSlide', 'forthSlide'],
-		showActiveTooltip: true,
-		slidesNavigation: true,
-		slidesNavPosition: 'bottom',
-      scrollBar: true,//是否包含滚动条，默认为false,若为true浏览器自带滚动条出现
-      scrollOverflow: true,//内容超过满屏后是否显示滚动条，true则显示滚动条，若需滚动查看内容还需要jquery.slimscroll插件的配合
-      scrollingSpeed: 500,
-      controlArrows:true,
-      dragAndMove:true,
-      loopBottom:true,
-      scrollOverflow:true,
-      onLeave: function (index, nextIndex, direction) {
-          console.log(index, nextIndex)
-          if (nextIndex == 2 ) {
-$(".OnlyFourSshow").show()
-          if (index > nextIndex) {
-              $('#current_num').html(index - 1);}
-          } else {
-              $('#current_num').html(nextIndex);
-          }
-      }
-  });
-    $('.next_page').on('click', function () {
-        $.fn.fullpage.moveSectionDown();
-    })
-
-
+$(function(){
+	$(document).ready(function() {
+        $('#fullpage').fullpage({
+            //Navigation
+            menu: '#menu',
+            lockAnchors: false,
+            anchors:['firstPage', 'secondPage', 'thirdPage', 'forthPage', 'fifthPage'],
+            navigation: true,
+            navigationPosition: 'right',
+            // navigationTooltips: ['首页', '第二页', '第三页', '第四页', '第五页'],
+            showActiveTooltip: true,
+            slidesNavigation: true,
+            slidesNavPosition: 'bottom',
+            scrollingSpeed:500,
+            controlArrows: true,
+            onLeave: function(index, nextIndex, direction){
+                console.log(index, nextIndex)
+                if(nextIndex==3||nextIndex==4){
+                    $(".next_page").find("a").css("color","#000");
+                    $("#scro").attr('src','src/images/down2.png');
+                    $("#fp-nav").find("span").css("background","rgb(101,121,145)");
+                    $("#fp-nav").find(".active").children("span").css("background","rgba(101,121,145)");
+                    $("#fp-nav").find("span").css("background","rgb（0,0,0,0.3）");
+                    $("#fp-nav").find(".active").children("span").css("background","#657991");
+                    $(".next_page").show()
+                }else{
+                    if(nextIndex==5){$(".next_page").hide()}
+                    else {$(".next_page").show()};
+                    $("#fp-nav").find("span").css("background","rgb(255,255,255)");
+                    $("#fp-nav").find(".active").children("span").css("background","rgba(255,255,255)");
+                    $(".next_page").find("a").css("color","#fff");
+                    $("#scro").attr('src','src/images/down.png');
+                }
+                if(index>nextIndex){
+                    $('#current_num').html(index-1);
+                }else {
+                    $('#current_num').html(nextIndex);
+                }
+            },
+        });
+        $('.next_page').on('click', function() {
+            $.fn.fullpage.moveSectionDown();
+        })
+    });
 })
