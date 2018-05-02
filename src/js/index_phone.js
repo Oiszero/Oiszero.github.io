f90c042cb51e550b8d692557ce0c3be342867097
Oiszero <172682271kzm@gmail.com>
2018-05-02 17:32:10 +0800

    no message

--- src/js/index_phone.js
+++ src/js/index_phone.js
@@ -1,7 +1,7 @@
 
 $(function() {
     $(document).ready(function () {
-        alert("test_iphone5_4")
+        alert("test_iphone5_5")
 
         $('#fullpage').fullpage({
             //Navigation
