dc3b36aa16b50456c38c173d47efaf23ba63c64a
Oiszero <172682271kzm@gmail.com>
2018-05-02 11:36:51 +0800

    no message

--- src/js/index_phone.js
+++ src/js/index_phone.js
@@ -23,6 +23,8 @@ $(function() {
             onLeave: function (index, nextIndex, direction) {
                 console.log(index, nextIndex)
                 if (nextIndex == 4 || nextIndex == 5 || nextIndex == 6) {
+                    var navtop=$(".page3_box").height()+$(".fourpage").height()
+                     $(".fp-slidesNav.bottom").css("top",navtop)
                     $(".next_page").find("a").css("color", "#000");
                     $("#scro").attr('src', 'src/images/down2.png');
                     $("#fp-nav").find("span").css("background", "rgb(101,121,145)");
